922a6511809777965e88429ecd27e2d99285e433
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-05-21 00:15:35 +0200

    Fix vector name  mistype

--- descriptive-statistics/sort.m
+++ descriptive-statistics/sort.m
@@ -4,10 +4,10 @@
 vector = [3 2 4 1 5 9 7 6];
 
 % Sort the vector in ascending order
-vector_asc = sort(v);
+vector_asc = sort(vector);
 
 % Sort the vector in descending order
-vector_desc = sort(v, 'descend');
+vector_desc = sort(vector, 'descend');
 
 % Print the results on the screen
 disp('Vector sorted in ascending order:');
