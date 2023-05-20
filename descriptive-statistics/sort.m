2fac0b7b525cfb7e9ab565ddf4d8836d51fe6e83
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-05-21 00:07:59 +0200

    Add vector sorting example

---
+++ descriptive-statistics/sort.m
@@ -0,0 +1,16 @@
+% Sort a vector in ascending and descending order
+
+% Create an exemplary vector
+vector = [3 2 4 1 5 9 7 6];
+
+% Sort the vector in ascending order
+vector_asc = sort(v);
+
+% Sort the vector in descending order
+vector_desc = sort(v, 'descend');
+
+% Print the results on the screen
+disp('Vector sorted in ascending order:');
+disp(vector_asc);
+disp('Vector sorted in descending order:');
+disp(vector_desc);
