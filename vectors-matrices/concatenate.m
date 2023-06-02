a76814a64aa231f002d52b25d3c761cb24b647b4
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-06-02 23:57:39 +0200

    Add example for concatenating vectors

---
+++ vectors-matrices/concatenate.m
@@ -0,0 +1,9 @@
+% Concatenate two row vectors
+row_vector_1 = [1 2];
+row_vector_2 = [3 4];
+matrix_1 = [row_vector_1 row_vector_2];
+
+% Concatenate two column vectors
+column_vector_1 = [1; 2];
+column_vector_2 = [3; 4];
+matrix_2 = [column_vector_1; column_vector_2];
