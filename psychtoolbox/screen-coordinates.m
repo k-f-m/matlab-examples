fea8ca660df4420268d998e77a7669e961001ba0
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-06-23 23:58:46 +0200

    Add example to get the cursor position on screen

---
+++ psychtoolbox/screen-coordinates.m
@@ -0,0 +1,27 @@
+% Example code to get mouse position on screen.
+
+function ScreenCoordinatesDemo
+    % Open a window and draw some text into it
+    window = Screen('OpenWindow', 0);
+    Screen('TextSize', window, 24);
+    DrawFormattedText(window, 'Move the cursor around the screen.', 'center', 'center', 0);
+    
+    while true
+        % Get current mouse position (x,y)
+        [x,y] = GetMouse;
+        
+        % Draw text with x,y position
+        DrawFormattedText(window, sprintf('x=%d y=%d', round(x), round(y)), 'center', 'center', 0);
+        
+        % Flip window to show text
+        Screen('Flip', window);
+        
+        % Check for ESC key press
+        if KbCheck
+            break;
+        end;
+    end;
+    
+    % Close window
+    Screen('CloseAll');
+end
