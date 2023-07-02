92ee9439fdd3407cbbfe2813f2ce84f75982a83c
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-07-03 00:41:11 +0200

    Complete code for semi-transparent-shapes.m

--- psychtoolbox/semi-transparent-shapes.m
+++ psychtoolbox/semi-transparent-shapes.m
@@ -12,3 +12,27 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
 % Create a base rectangle of 500 by 500 pixels
 baseRect = [0 0 500 500];
+
+% Set screen positions and the number of rectangles
+xPosition = [xCenter - 250 xCenter + 250 xCenter - 250 xCenter +250];
+yPosition = [yCenter yCenter yCenter - 250 yCenter + 250];
+numSquares = length(xPosition);
+
+% Set RGBA values of rectangles
+rgbaValues = [0 1 0 1; 1 0 0 1; 1 1 0 0.4; 1 0 1 0.4]';
+
+ % Set coordinates of rectangles
+ rectCoordinates = nan(4, 4);
+ for i = 1:numSquares
+     allRects(:, i) = CenterRectOnPointd(baseRect, xPosition(i), yPosition(i));
+ end
+
+% Draw rectangles on the screen
+Screen('FillRect', window, rgbaValues, rectCoordinates);
+
+% Flip all previous commands to the screen
+Screen('Flip', window);
+
+% Wait for a key press and clear the screen
+KbStrokeWait;
+sca;
